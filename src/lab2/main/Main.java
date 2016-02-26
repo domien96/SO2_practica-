736c56e7430eda3445f153dab341a6414d3f5efd
Domien Van Steendam <domien.vansteendam@ugent.be>
2016-02-26 10:23:08 +0100

    opgave 1 gemaakt.

--- src/lab2/main/Main.java
+++ src/lab2/main/Main.java
@@ -13,17 +13,34 @@ public class Main {
         
         String[] names = new String[]{"Jan", "Piet", "Joris", "Corneel"};
         
-        OrderProcessor orderProcessor = new OrderProcessor();  
+        OrderProcessor orderProcessor = new OrderProcessor();
+
+        Thread[] threads = new Thread[names.length];
+        int threadIdx = 0;
 
         for(String name : names){
             Customer customer = new Customer(name);
             Thread t = new Thread(() -> {
                 for(int i=0;i<noOrders;i++){
                     customer.buy("item-"+i);
+
                 }
             });
+            //t.setName(name); // controle
+            threads[threadIdx++] = t;
             t.start();
         }
+
+        // Wait for each thread
+        for(Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        //All threads have done their work from now on.
+        System.out.println(OrderProcessor.getNumberOfOrders());
     }
     
 }
