dbc3be7df7ef4847913b509abb4d1f2468e7f42d
jjocqu <jens.jocque@ugent.be>
2016-03-25 00:24:20 +0100

    connectionlistener implemented

--- src/lab3/network/ConnectionListener.java
+++ src/lab3/network/ConnectionListener.java
@@ -1,16 +1,47 @@
 package lab3.network;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 // listens for incoming connections
 public class ConnectionListener extends Thread {
-    public ConnectionListener(Network network, int serverPort) {
 
+    private Network network;
+    private int serverPort;
+    private ServerSocket listen = null;
+    private Socket socket = null;
+    private boolean stop;
+
+    public ConnectionListener(Network network, int serverPort) {
+        this.network = network;
+        this.serverPort = serverPort;
+        stop = false;
     }
 
     public void run() {
-
+        while(!stop) { //blijven runnen tot terminate opgeroepen wordt
+            try {
+                listen = new ServerSocket(serverPort);
+                socket = listen.accept();
+                if (socket != null) { //client gevonden
+                    network.connect(socket);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (listen != null) {
+                    try {
+                        listen.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 
     public void terminate() {
-
+        stop = true;
     }
 }
