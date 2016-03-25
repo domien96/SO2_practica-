8198621656c7db227f25b0155e140bcceec22ab1
Domien Van Steendam <domien.vansteendam@ugent.be>
2016-03-25 10:53:01 +0100

    jerry.

---
+++ src/lab4/chat/ChatController.java
@@ -0,0 +1,15 @@
+package lab4.chat;
+
+/**
+ * Created by domien on 25/03/2016.
+ */
+public class ChatController {
+
+    public ChatController() {
+
+    }
+
+    private void initialize() {
+        
+    }
+}
