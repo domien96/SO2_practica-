4a4a2e3a07984a1a1e11d5061a21a4bc38f8d849
Domien Van Steendam <domien.vansteendam@ugent.be>
2016-04-15 09:58:24 +0200

    poesjes

---
+++ src/lab5/othelllo/Main.java
@@ -0,0 +1,31 @@
+package lab5.othelllo;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ * Created by domien on 15/04/2016.
+ */
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GridPane root = new GridPane();
+        primaryStage.setScene(new Scene(root,300,300));
+
+        root.add(new OthelloPiece().getContent(),0,0);
+        root.add(new OthelloPiece().getContent(),0,1);
+        root.add(new OthelloPiece().getContent(),1,0);
+        root.add(new OthelloPiece().getContent(),1,1);
+
+        primaryStage.setTitle("MainOthelloPiece");
+        root.setVgap(10);
+        root.setHgap(10);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/lab5/othelllo/OthelloPiece.java
+++ src/lab5/othelllo/OthelloPiece.java
@@ -2,16 +2,37 @@ package lab5.othelllo;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+import java.io.Serializable;
 
 /**
  * Created by domien on 15/04/2016.
  */
-public class OthelloPiece
+public class OthelloPiece implements Serializable
 {
     private ObjectProperty<Integer> state = new SimpleObjectProperty<>(0);
+    private Pane cell = new Pane();
+    private Circle[] circles = { new Circle(50, Color.BLACK), new Circle(50,Color.GRAY), new Circle(50,Color.WHITE)};
 
     public OthelloPiece(){
         // intialisatie ...
+        cell.setStyle("-fx-background-color: gray");
+        cell.setPrefSize(50,50);
+        cell.setMinSize(50,50);
+        // default grijze cirkel
+        cell.setOnMouseClicked((e) -> {
+            if (this.getFoo() == -1) {
+                this.setFoo(1);
+            } else {
+                this.setFoo(getFoo() - 1);
+            }
+        }
+        );
+
     }
 
     public synchronized Integer getFoo(){
@@ -20,9 +41,15 @@ public class OthelloPiece
 
     public synchronized void setFoo(int newValue){
         state.setValue(newValue);
+        cell.setBackground(new);
+        cell.getChildren().add(circles[newValue+1]);
     }
     
     public ObjectProperty<Integer> fooProperty() {
         return state;
+    }
+
+    public Pane getContent() {
+        return cell;
     }
 }
