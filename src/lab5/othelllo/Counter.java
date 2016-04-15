d150506c4800cc608e241c3ba4ca84991ae39548
Domien Van Steendam <domien.vansteendam@ugent.be>
2016-04-15 10:52:21 +0200

    poesjes

--- src/lab5/othelllo/Counter.java
+++ src/lab5/othelllo/Counter.java
@@ -1,9 +1,62 @@
 package lab5.othelllo;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
 import java.io.Serializable;
 
 /**
  * Created by domien on 15/04/2016.
  */
 public class Counter implements Serializable {
+    private ObjectProperty<Integer> number = new SimpleObjectProperty<>(0);
+    private ObjectProperty<String> stateToCount = new SimpleObjectProperty<>("default");
+
+    private GridPane cell = new GridPane();
+
+    public Counter() {
+        // initialize...
+        cell.add(new Label(this.stateToCount.get()),0,0);
+        cell.add(new Label(this.number.get().toString()),0,1);
+    }
+
+    public synchronized Integer getNumber() {
+        return number.get();
+    }
+
+    public ObjectProperty<Integer> numberProperty() {
+        return number;
+    }
+
+    public synchronized void setNumber(Integer number) {
+        this.number.set(number);
+    }
+
+    public synchronized String getStateToCount() {
+        return stateToCount.get();
+    }
+
+    public ObjectProperty<String> stateToCountProperty() {
+        return stateToCount;
+    }
+
+    public synchronized void setStateToCount(String stateToCount) {
+        this.stateToCount.set(stateToCount);
+    }
+
+    public void increment() {
+        this.setNumber(this.getNumber()+1);
+    }
+
+    public void decrement() {
+        this.setNumber(this.getNumber()-1);
+    }
+
+    public Pane getContent() {
+        return cell;
+    }
 }
