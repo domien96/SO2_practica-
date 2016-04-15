6e4d4cfc58d29c11f274696ea5b1dc365d0c371b
jjocqu <jens.jocque@ugent.be>
2016-04-15 09:29:44 +0200

    pushes
    pipi

--- src/lab5/othelllo/OthelloPiece.java
+++ src/lab5/othelllo/OthelloPiece.java
@@ -1,8 +1,28 @@
 package lab5.othelllo;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
 /**
  * Created by domien on 15/04/2016.
  */
 public class OthelloPiece
 {
+    private ObjectProperty<Integer> state = new SimpleObjectProperty<>(0);
+
+    public OthelloPiece(){
+        // intialisatie ...
+    }
+
+    public synchronized Integer getFoo(){
+        return state.getValue();
+    }
+
+    public synchronized void setFoo(int newValue){
+        state.setValue(newValue);
+    }
+    
+    public ObjectProperty<Integer> fooProperty() {
+        return state;
+    }
 }
